e7acb998d94a0d2b8ea391ff8461deb80e20056c
unikorm <adaled00@gmail.com>
2023-06-07 20:05:47 +0200

    tak chatGPT mi vyplul zaklad, teraz to cele prekopem po vlastnom, ale AI mi este vela pomoze, spolieham sa na nu

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+  
+    var contactForm = document.getElementById("contactForm");
+    contactForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      // Add your logic here to handle form submission and sending email
+      // You can use JavaScript or any server-side language for this
+      // For example, you can make an AJAX request or redirect to a specific URL
+    });
+  });
+  
