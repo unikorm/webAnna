a66f9ae285f99150d3f4ebbbb4010ea346a4a7e5
unikorm <adaled00@gmail.com>
2023-06-10 20:34:05 +0200

    no uz som dlho neurobil commmmit, uz pracujem na js funkcionalitach, nebavilo ma css upravovat teraz tak som zacat sediviet pri JS, dobre mi tak

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-  document.addEventListener("DOMContentLoaded", function() {
-  console.log("kkt")
+
+
+
+  let contactContainer = document.querySelector(".contactContainer");
+  let mainContainer = document.querySelector(".mainContainer");
+  
+  // Event Handlers
+
+  // Open the contact form when "Contact" link is clicked
+let contactLink = document.getElementsByClassName("navbar-nav")[0].getElementsByTagName("a")[2];
+console.log(contactLink);
+if (contactLink) {
+  contactLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    openContactForm();
+    console.log("kktina")
   });
-  
-  
+};
+
+
+  // Functions
+
+  // Function to open the contact form
+function openContactForm() {
+  mainContainer.style.display = "none";
+  contactContainer.style.display = "flex";
+};
+
+
