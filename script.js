6b2453f11086677d9246f702e1088893165e6edc
unikorm <adaled00@gmail.com>
2023-06-10 23:39:59 +0200

    okay, popridaval som tam funkcie na spravne fungovanie contact form, prevetral som si JS, ale chtGPT mi dost pomohol, je pravda ze mu to vyslo az na siesty o pokus

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
+// variables ######@~$&*
+
+let contactContainer = document.querySelector(".contactContainer");
+let mainContainer = document.querySelector(".mainContainer");
+let contactLink = document.getElementsByClassName("navbar-nav")[0].getElementsByTagName("li")[2];
+let backButton = document.getElementsByClassName("buttons")[0].getElementsByTagName("button")[1];
 
 
 
-  let contactContainer = document.querySelector(".contactContainer");
-  let mainContainer = document.querySelector(".mainContainer");
-  
-  // Event Handlers
 
-  // Open the contact form when "Contact" link is clicked
-let contactLink = document.getElementsByClassName("navbar-nav")[0].getElementsByTagName("a")[2];
-console.log(contactLink);
+// Event Handlers ######@~$&*
+
+// Open the contact form when "Contact" link is clicked
 if (contactLink) {
-  contactLink.addEventListener('click', function(event) {
+  contactLink.addEventListener("click", function(event) {
     event.preventDefault();
     openContactForm();
-    console.log("kktina")
+  });
+};
+
+// Close the contact form when Esc key is pressed
+document.addEventListener("keydown", function(event) {
+  if (event.key === 'Escape') {
+    closeContactForm();
+  };
+});
+
+// Close the contact form when clicked outside of it
+document.addEventListener("click", function(event) {
+  let targetElement = event.target;
+
+  if (contactContainer.style.display = "flex" && contactContainer.contains(targetElement) && !targetElement.closest(".contact-form")) {
+    closeContactForm();
+  };  
+});
+
+// Close the contact form when "Back" button is clicked
+if (backButton) {
+  backButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    closeContactForm();
   });
 };
 
 
-  // Functions
 
-  // Function to open the contact form
+
+// Functions ######@~$&*
+
+// Function to open the contact form
 function openContactForm() {
-  mainContainer.style.display = "none";
   contactContainer.style.display = "flex";
+  mainContainer.style.display = "none";
 };
 
-
+// Function to close the contact form
+function closeContactForm() {
+  mainContainer.style.display = "block";
+  contactContainer.style.display = "none";
+};
