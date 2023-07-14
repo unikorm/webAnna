ed7f83b59e150c2465ccd52ffe7c112eb696e09c
unikorm <adaled00@gmail.com>
2023-07-14 13:29:43 +0200

    dobre, teraz to bude na dlhsie kedze ja to nechcem len spojazdnit ale tomu aj lepsie pochopit a vediet sa v tom vyznat, takze robim na odosielani kontaktneho formularu na moj email, ponbozko mi pomahaj

--- src/php_stuff/send_email.php
+++ src/php_stuff/send_email.php
@@ -1,5 +1,7 @@
+<!-- this is from Github -->
+
 <?php
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -24,4 +26,33 @@ try {
     echo 'Email sent successfully';
 } catch (Exception $e) {
     echo 'Email could not be sent. Error: ' . $mail->ErrorInfo;
-}
+};
+
+
+// a toto su moje skusky co poskladam z internetu
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer/PHPMailer/PHPMailer();
+
+$mail->isSMTP();  // Set mailer to use SMTP
+$mail->Host = 'smtp.example.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;  // Enable SMTP authentication
+$mail->Username = 'your_email@example.com';  // SMTP username
+$mail->Password = 'your_password';  // SMTP password
+$mail->SMTPSecure = 'tls';  // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;  // TCP port to connect to
+
+$mail->setFrom('your_email@example.com', 'Your Name');
+$mail->addAddress('recipient@example.com', 'Recipient Name');
+$mail->Subject = 'Subject of your email';
+$mail->Body = 'Content of your email';
+
+if ($mail->send()) {
+    echo 'Email sent successfully!';
+} else {
+    echo 'Email could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+};
+
+
