9beb0e8342f662c1db5f21085ad80a24219c45d0
unikorm <adaled00@gmail.com>
2023-07-19 18:28:48 +0200

    little changes to code, does not work very well fuck

--- src/php_stuff/send_email.php
+++ src/php_stuff/send_email.php
@@ -1,39 +1,7 @@
-<!-- this is from Github  -->
-
-
-/* require '../vendor/autoload.php';
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-$mail = new PHPMailer(true);
-
-$mail->isSMTP();
-$mail->Host = 'smtp.example.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'your-email@example.com';
-$mail->Password = 'your-password';
-$mail->SMTPSecure = 'tls';
-$mail->Port = 587;
-
-$mail->setFrom('your-email@example.com', 'Your Name');
-$mail->addAddress('recipient@example.com', 'Recipient Name');
-$mail->Subject = 'Contact Form Submission';
-$mail->Body = $_POST['message']; // Assuming your form has an input field named 'message' for the message content
-
-try {
-    $mail->send();
-    echo 'Email sent successfully';
-} catch (Exception $e) {
-    echo 'Email could not be sent. Error: ' . $mail->ErrorInfo;
-}; */
-
-
-// a toto su moje skusky co poskladam z internetu
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 
-require '../vendor/autoload.php';
+require '../../../vendor/autoload.php';
 
 $mail = new PHPMailer();
 
