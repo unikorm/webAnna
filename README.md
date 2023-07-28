7818222adf34484b829796f72753a1b7ddebc798
unikorm <adaled00@gmail.com>
2023-07-28 18:29:52 +0200

    it is fucking workin

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-*Photography website for my fionceé to reach more clients and simplify the process of sending her orders. 
+*Photography website for my wife already to reach more clients and simplify the process of sending her orders. 
 Design of the site will be very simply and minimalistic with contact form, portfolia section with some her top photos, some bio - information about her, homepage with big wallpaper, etc.*
--- src/php_stuff/send_email.php
+++ src/php_stuff/send_email.php
@@ -3,8 +3,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require "/Users/adamaanna/Documents/www/webAnna/vendor/autoload.php";
-require "/Users/adamaanna/Documents/www/webAnna/src/php_stuff/config.php";
+require $_SERVER['DOCUMENT_ROOT'] . "/webAnna/vendor/autoload.php";
+require $_SERVER['DOCUMENT_ROOT'] . "/webAnna/src/php_stuff/config.php";
+
 
 // Check if the form data has been sent using POST method
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
