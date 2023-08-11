07e4578504073d92f3b73d22e17d9e4faacad566
unikorm <adaled00@gmail.com>
2023-08-11 20:26:06 +0200

    okey, urobil som si maly rozbor kodu, co co robi a nerobi, a neskor sa na to pozriem

--- src/JS/script.js
+++ src/JS/script.js
@@ -4,15 +4,6 @@
 import { contactContainer, mainContainer, contactLink, backButton, aboutLink, aboutContainer, menuLink, menuContainer, portfolioContainer, portfolioLink, pricingContainer, pricingLink, nameInput, emailInput, messageInput, errorMessage, successMessage} from "/src/JS/variables.js";
 let currentOpenSection = getSectionContainerById(getURLSectionId());  // navigation logic variable
 
-// function to after reload of page clean queries history and start from clean state
-document.addEventListener("DOMContentLoaded", function () {
-  if (window.performance && window.performance.navigation.type === window.performance.navigation.TYPE_RELOAD) {
-    let url = new URL(window.location.href);
-    url.searchParams.delete("section");
-    window.history.replaceState(null, null, url);
-  }
-});
-
 
 // general reuseble functions
 function openSection(sectionContainer) {
@@ -172,6 +163,9 @@ document.addEventListener("keydown", function (event) {
 
 
 // logic to updateing URL address
+
+//this works
+// this function put new query in URL after opening new section, put it into history too
 function updateURL(sectionId) {
   let url = new URL(window.location.href);
 
@@ -183,6 +177,7 @@ function updateURL(sectionId) {
   history.pushState(null, null, url);
 };
 
+// this function get name of id and give in result them name of variable
 function getSectionContainerById(sectionId) {
   switch (sectionId) {
     case "menuContainer":
@@ -200,6 +195,17 @@ function getSectionContainerById(sectionId) {
   };
 };
 
+// this function return a string, value of URL query parameter named "section" 
+function getURLSectionId() {
+  let urlParams = new URLSearchParams(window.location.search);
+  let sectionId = urlParams.get("section");
+  return sectionId;
+};
+
+
+
+// this i think doesn't work properly
+// this function do something like open/close sections, but it cross with other logic doin it i think, and that is problem... maybe
 function navigateToSectionFromURL() {
   let urlParams = new URLSearchParams(window.location.search);
   let sectionId = urlParams.get("section");
@@ -220,12 +226,16 @@ function navigateToSectionFromURL() {
   };
 };
 
-function getURLSectionId() {
-  let urlParams = new URLSearchParams(window.location.search);
-  let sectionId = urlParams.get("section");
-  return sectionId;
-};
+// function to after reload of page clean queries history and start from clean state (it seems like it doesn't work well now)
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.performance && window.performance.navigation.type === window.performance.navigation.TYPE_RELOAD) {
+    let url = new URL(window.location.href);
+    url.searchParams.delete("section");
+    window.history.replaceState(null, null, url);
+  }
+});
 
+// this function listen on click on back/forward and calling function "n" and redeclare let "c" (idk what exactly do it)
 window.addEventListener("popstate", () => {
   navigateToSectionFromURL();
   currentOpenSection = getSectionContainerById(getURLSectionId());
